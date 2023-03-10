69d9241adc9b82699eb036eb684f347a1ccf7a34
Tarciso Bandeira <tarcisojb@gmail.com>
2023-03-10 11:21:04 -0300

    aula19

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import { Component } from 'react';
+import { React, Component } from 'react';
 import './App.css';
 
-class App extends Component() {
+class App extends Component {
   state = {
     posts: [
 
     ]
   };
 
-  render(){
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
+
+    this.setState({ posts: postsPhotos })
+  }
+
+  render() {
+    const { posts } = this.state;
+
     return (
-      <div className="App">
-  
-      </div>
+      <section className='container'>
+        <div className="posts">
+          {posts.map(post => (
+            <div className='post'>
+              <img src={post.cover} alt={post.title}/>
+              <div key={post.id} 
+              className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
